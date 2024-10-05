13d352bc5dcbbc0c47e8c649b0d8c8eed46dafd4
peccu <peccul@gmail.com>
2024-10-05 11:59:36 +0000

    cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
 "use client";
-import FeedReader from "@/components/feed-reader";
 
 export default function Home() {
-  return <FeedReader />;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <ul className="flex flex-col items-center justify-center">
+        <li className="text-2xl font-bold">
+          <a href="/nodes-and-edge">Nodes and Edge</a>
+        </li>
+      </ul>
+    </div>
+    )
+  ;
 }
